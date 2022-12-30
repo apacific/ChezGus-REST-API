4f30aa0d7c00b0fd74dfbe0db885b04cba87037d
Anthony Pacific <apacific@catalyte.io>
2022-12-29 20:33:07 -0800

    initial commit

---
+++ src/main/java/io/catalyte/apacific/chezgus/services/AppRunner.java
@@ -0,0 +1,15 @@
+package io.catalyte.apacific.chezgus.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starts the spring application
+ */
+@SpringBootApplication
+public class AppRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(AppRunner.class, args);
+    }
+}
+
