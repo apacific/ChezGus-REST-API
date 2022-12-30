fea8372ddea06a15894483c1102c1266e07e7591
Anthony Pacific <apacific@catalyte.io>
2022-12-30 13:26:57 -0800

    update pom file, add yaml file, update string constants, update entities

---
+++ src/main/java/io/catalyte/apacific/chezgus/constants/StringConstants.java
@@ -0,0 +1,10 @@
+package io.catalyte.apacific.chezgus.constants;
+
+public class StringConstants {
+
+    //validation constants
+    public static final String GENERATED_ID = "The database generated ID";
+    public static final String ITEM_DESCRIPTION = "Description of the menu item";
+    public static final String ITEM_NAME = "Name of the menu item";
+    public static final String REQUIRED_FIELD = "Field is required";
+}
--- src/main/java/io/catalyte/apacific/chezgus/entities/Item.java
+++ src/main/java/io/catalyte/apacific/chezgus/entities/Item.java
@@ -1,8 +1,34 @@
 package io.catalyte.apacific.chezgus.entities;
 
+import javax.persistence.*;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.math.BigDecimal;
+import javax.validation.constraints.NotNull;
+
+import static io.catalyte.apacific.chezgus.constants.StringConstants.*;
+
 /**
  * Item entity and associated attributes.
  * Many-to-many relationship with Order.
  */
+@Entity
+@Table(name = "items")
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(notes = GENERATED_ID)
+    private Long id;
+
+    @ApiModelProperty(notes = ITEM_NAME)
+    private String name;
+
+    @ApiModelProperty(notes = ITEM_DESCRIPTION)
+    private String description;
+
+    @NotNull(message = REQUIRED_FIELD)
+    @ApiModelProperty(notes = "Item price in USD",
+            required = true)
+    private BigDecimal price;
 }
