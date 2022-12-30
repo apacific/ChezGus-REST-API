2d79c589e556a4f9d7a3c43b8d5b40a05cf29b29
Anthony Pacific <apacific@catalyte.io>
2022-12-30 13:28:33 -0800

    add swagger configuration

---
+++ src/main/java/io/catalyte/apacific/chezgus/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package io.catalyte.apacific.chezgus.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Class that allows swagger documentation for project
+ */
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).pathMapping("/")
+                .useDefaultResponseMessages(false).select().apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error")))//Exclude Spring error controllers
+                .build();
+    }
+
+
+}
