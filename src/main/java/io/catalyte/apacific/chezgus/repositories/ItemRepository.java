703f318871d404694a54ea6bef28eab1924beecd
Anthony Pacific <apacific@catalyte.io>
2022-12-30 13:38:43 -0800

    update item repository

--- src/main/java/io/catalyte/apacific/chezgus/repositories/ItemRepository.java
+++ src/main/java/io/catalyte/apacific/chezgus/repositories/ItemRepository.java
@@ -1,2 +1,15 @@
-package io.catalyte.apacific.chezgus.repositories;public class ItemRepository {
+package io.catalyte.apacific.chezgus.repositories;
+
+
+import io.catalyte.apacific.chezgus.entities.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository to store menu items.
+ */
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    boolean existsByItemId(Long itemId);
 }
