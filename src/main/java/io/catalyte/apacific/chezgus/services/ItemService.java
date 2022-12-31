610a9a8fa7be5f714292fb8a28b80ae952614346
Anthony Pacific <apacific@catalyte.io>
2022-12-30 22:49:46 -0800

    update project file structure

--- src/main/java/io/catalyte/apacific/chezgus/services/ItemService.java
+++ src/main/java/io/catalyte/apacific/chezgus/services/ItemService.java
@@ -1,2 +1,20 @@
-package io.catalyte.apacific.chezgus.services;public class ItemService {
+package io.catalyte.apacific.chezgus.services;
+
+import io.catalyte.apacific.chezgus.entities.Item;
+
+import java.util.List;
+
+public interface ItemService {
+
+/**
+ * item service interface with create and update methods for an item
+ */
+
+    List<Item> getItems(Long orderId);
+
+    Item getItemById(Long orderId, Long id);
+
+    Item addItem(Long orderId, Item item);
+
+    Item updateItemById(Long orderId, Long id, Item item);
 }
