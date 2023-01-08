52f56b3615ba058b40c846d92252ccb988896ba0
Anthony Pacific <apacific@catalyte.io>
2023-01-08 03:17:31 -0800

    linting

--- src/main/java/io/catalyte/apacific/chezgus/controllers/ItemController.java
+++ src/main/java/io/catalyte/apacific/chezgus/controllers/ItemController.java
@@ -30,7 +30,6 @@ import static io.catalyte.apacific.chezgus.constants.StringConstants.*;
         @ApiResponse(code = 503, message = SERVICE_UNAVAILABLE, response = HttpServerErrorException.ServiceUnavailable.class)})
 
 public class ItemController {
-
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
 
     @Autowired
@@ -47,17 +46,15 @@ public class ItemController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = HTTP_200, responseContainer = "List",
             response = Item.class)})
     public ResponseEntity<List<Item>> getItems(@PathVariable Long orderId) {
-        logger
-                .info(new Date() + " Get request received of items for patient with id: " + orderId);
-
+        logger.info(new Date() + " Get request received of items for patient with id: " + orderId);
         return new ResponseEntity<>(itemService.getItems(), HttpStatus.OK);
     }
 
     /**
-     * Gets item by id for a order in the database
+     * Gets item by id for an order in the database
      *
      * @param orderId the id of the order connected to the item from the path variable
-     * @param id        the item's id from the path variable
+     * @param id      the item's id from the path variable
      * @return the item with the same id
      */
     @GetMapping(value = ID_ENDPOINT)
@@ -67,7 +64,6 @@ public class ItemController {
     public ResponseEntity<Item> getItem(@PathVariable Long orderId,
                                         @PathVariable Long id) {
         logger.info(new Date() + " Get by id: " + id + " for order with id: " + orderId);
-
         return new ResponseEntity<>(itemService.getItemById(id), HttpStatus.OK);
     }
 
@@ -75,7 +71,7 @@ public class ItemController {
      * Adds new item to the order
      *
      * @param orderId the id of the order connected to the item from the path variable
-     * @param item the item from the request body being added
+     * @param item    the item from the request body being added
      * @return the item if added correctly
      */
     @PostMapping
@@ -83,9 +79,8 @@ public class ItemController {
     @ApiResponses(value = {@ApiResponse(code = 201, message = "CREATED", response = Item.class),
             @ApiResponse(code = 400, message = "BAD DATA")})
     public ResponseEntity<Item> addItem(@PathVariable Long orderId,
-                                                  @Valid @RequestBody Item item) {
+                                        @Valid @RequestBody Item item) {
         logger.info(new Date() + "  Item POST request received");
-
         return new ResponseEntity<>(itemService.addItem(item),
                 HttpStatus.CREATED);
     }
@@ -94,7 +89,7 @@ public class ItemController {
      * Updates an item within the database
      *
      * @param orderId the id of the order connected to the item from the path variable
-     * @param id        the id of the item to updated from the path variable
+     * @param id the id of the item to updated from the path variable
      * @param item the item's new information from the request body
      * @return the item if correctly updated
      */
@@ -104,9 +99,8 @@ public class ItemController {
             @ApiResponse(code = 400, message = "BAD DATA"),
             @ApiResponse(code = 404, message = "NOT FOUND")})
     public ResponseEntity<Item> updateItemById(@PathVariable Long orderId,
-                                                         @PathVariable Long id, @Valid @RequestBody Item item) {
+                                               @PathVariable Long id, @Valid @RequestBody Item item) {
         logger.info(new Date() + " Item PUT request received for id: " + id);
-
         return new ResponseEntity<>(itemService.updateItemById(id, item),
                 HttpStatus.OK);
     }
